201a60b65ecdd5c1000b8f5eac04669a33b41c24
nataliia-pysanka <pysankanataliia@gmail.com>
2022-10-04 13:28:36 +0300

    login

--- src/repository/users.py
+++ src/repository/users.py
@@ -3,9 +3,33 @@ from src import models
 import bcrypt
 
 
-def create_user(email, password):
-    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(rounds=10))
+def create_user(email: str, password: str):
+    hash_pass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(
+        rounds=10))
+    hashed = hash_pass.decode('utf-8')
+
     user = models.User(email=email, hash=hashed)
     db.session.add(user)
     db.session.commit()
+    return user
+
+
+def login(email: str, password: str):
+    user = find_by_email(email)
+    if not user:
+        return None
+    if not bcrypt.checkpw(password.encode('utf-8'), user.hash.encode('utf-8')):
+        return None
+    return user
+
+
+def find_by_email(email: str):
+    user = db.session.query(models.User).filter(models.User.email == email).\
+        first()
+    return user
+
+
+def find_by_id(ids: int):
+    user = db.session.query(models.User).filter(models.User.id == ids).\
+        first()
     return user
--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session, \
+    make_response
 from src import app
 from src.repository import users
 
@@ -10,13 +11,14 @@ def healthcheck():
 
 @app.route('/', methods=['GET', 'POST'], strict_slashes=False)
 def index():
+    auth = True if 'user' in session else False
     if request.method == 'POST':
         if request.form.get('contacts_input'):
             return redirect(url_for('contacts'))
         elif request.form.get('notes_input'):
             return redirect(url_for('notes'))
 
-    return render_template('pages/index.html')
+    return render_template('pages/index.html', auth=auth)
 
 
 @app.route('/login', methods=['GET', 'POST'], strict_slashes=False)
@@ -24,10 +26,20 @@ def login():
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
+        remember = request.form.get('remember')
+
         user = users.login(email, password)
         if user is None:
             return redirect(url_for('login'))
-    return redirect(url_for('index'))
+
+        session['user'] = {'email': user.email, "id": user.id}
+        response = make_response(redirect(url_for('index')))
+
+        if remember:
+            pass
+
+        return response
+    return render_template('pages/login.html')
 
 
 @app.route('/signin', methods=['GET', 'POST'], strict_slashes=False)
@@ -42,14 +54,25 @@ def signin():
 
 @app.route('/logout', strict_slashes=False)
 def logout():
-    return render_template('index.html')
+    auth = True if 'user' in session else False
+    if not auth:
+        return redirect(request.url)
+    session.pop('user')
+    response = make_response(redirect(url_for('index')))
+    return response
 
 
 @app.route('/contacts', strict_slashes=False)
 def contacts():
-    return render_template('pages/contacts.html')
+    auth = True if 'user' in session else False
+    if not auth:
+        return redirect(request.url)
+    return render_template('pages/contacts.html', auth=auth)
 
 
 @app.route('/notes', strict_slashes=False)
 def notes():
-    return render_template('pages/notes.html')
+    auth = True if 'user' in session else False
+    if not auth:
+        return redirect(request.url)
+    return render_template('pages/notes.html', auth=auth)
