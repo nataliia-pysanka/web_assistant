08a44311b4cd7a0d2f7c4a097762f67daa0dee01
nataliia-pysanka <pysankanataliia@gmail.com>
2022-10-29 22:44:34 +0300

    work with notes

--- src/models.py
+++ src/models.py
@@ -15,6 +15,9 @@ class User(db.Model):
     def __repr__(self):
         return f'User({self.username}, {self.email})'
 
+    def __str__(self):
+        return f'{self.username}'
+
 
 contact_group = db.Table('contact_group',
                          db.Column('contact_id', db.Integer,
@@ -43,6 +46,9 @@ class Contact(db.Model):
     def __repr__(self):
         return f"Contact({self.first_name} {self.last_name})"
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Phone(db.Model):
     __tablename__ = 'phone_table'
@@ -53,6 +59,9 @@ class Phone(db.Model):
     contact = relationship('Contact', back_populates='phones')
 
     def __repr__(self):
+        return f"Phone({self.phone})"
+
+    def __str__(self):
         return f"{self.phone}"
 
 
@@ -65,6 +74,9 @@ class Email(db.Model):
     contact = relationship('Contact', back_populates='emails')
 
     def __repr__(self):
+        return f"Email({self.email})"
+
+    def __str__(self):
         return f"{self.email}"
 
 
@@ -74,6 +86,28 @@ class Group(db.Model):
     name = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
+        return f"Group({self.name})"
+
+    def __str__(self):
         return f"{self.name}"
 
 
+note_tag = db.Table('note_tag',
+                    db.Column('note_id', db.Integer,
+                              db.ForeignKey('note_table.id')),
+                    db.Column('tag_id', db.Integer,
+                              db.ForeignKey('tag_table.id'))
+                    )
+
+
+class Note(db.Model):
+    __tablename__ = 'note_table'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    text = db.Column(db.String(1200), nullable=False)
+
+
+class Tag(db.Model):
+    __tablename__ = 'tag_table'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
